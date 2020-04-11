7581c3adcf8ec6eed5b0578f2563cb264a771a8e
smorgrav <torbjorn@smorgrav.org>
2020-04-11 17:02:59 +0200

    But react is needed

--- src/pages/PhotoGrid.js
+++ src/pages/PhotoGrid.js
@@ -1,3 +1,4 @@
+import React from "react";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
